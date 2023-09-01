7701dbdcf893adcb9f75fecf44fe05d94f56eb08
Thaliaraujo <thalia.araujo321@gmail.com>
2023-09-01 16:05:37 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -193,6 +193,9 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
+  function deletarColoborador() {
+    console.log('deletando colaborador');
+  };
 
   return (
     <div>
@@ -200,7 +203,15 @@ function App() {
       <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
       <section className="times">
         <h1>Minha organização</h1>
-        {times.map((time, indice) => <Time key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} />)}
+        {times.map((time, indice) => 
+          <Time 
+            key={indice} 
+            time={time} 
+            colaboradores={colaboradores.filter(colaborador => colaborador.time 
+            === time.nome)} 
+            aoDeletar={deletarColoborador}
+          />
+        )}
       </section>
       <Rodape />
     </div>
